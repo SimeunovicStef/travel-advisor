15a20c26215ca0302745d5913599e4afd2991ff4
SimeunovicStef <eskimofrogster@gmail.com>
2022-04-30 20:30:25 +0200

    header and map code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { CssBaseline, Grid } from "@material-ui/core";
-import Header from './components/Header/Header'
-import List from './components/List/List'
-import Map from './components/Map/Map'
-// import PlaceDetails from './components/PlaceDetails/PlaceDetails'
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
 
 
 
